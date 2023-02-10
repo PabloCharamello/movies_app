0c8b629708ff45612a4da35d7caca8e97ce3f784
Pablo Charamello <pablocharamello@gmail.com>
2023-02-10 20:23:27 -0300

    Adding base styles to app and .env added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="page">
+      <header>
+        <h1>Movie search app</h1>
+        <form className="form">
+          <input type="text" placeholder="The Matrix, Aliens, Armaggedon..." />
+          <button type="submit">Search</button>
+        </form>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
