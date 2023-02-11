c3df333c671e57e116a9c33382bea64b5bc35227
Pablo Charamello <pablocharamello@gmail.com>
2023-02-10 22:39:25 -0300

    Movies component added and custom hook useMovies created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
+import { useMovies } from "./hooks/useMovies";
+import { Movies } from "./components/Movies";
 
 function App() {
+  const { movies: mappedMovies } = useMovies();
   return (
     <div className="page">
       <header>
-        <h1>Movie search app</h1>
+        <h1>Movies search App</h1>
         <form className="form">
-          <input type="text" placeholder="The Matrix, Aliens, Armaggedon..." />
+          <input type="text" placeholder="The Matrix, Aliens, Armageddon..." />
           <button type="submit">Search</button>
         </form>
       </header>
+
+      <main>
+        <Movies movies={mappedMovies} />
+      </main>
     </div>
   );
 }
---
+++ src/components/Movies.jsx
@@ -0,0 +1,23 @@
+function ListOfMovies({ movies }) {
+  return (
+    <ul className="page">
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <h2>{movie.title}</h2>
+          <img src={movie.poster} alt={movie.title} />
+          <p>Year: {movie.year}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function NotMoviesResults() {
+  return <p>Not results found</p>;
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NotMoviesResults />;
+}
